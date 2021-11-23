e807c76b5d7952de536129c179189b80e85bea63
Emily Faccin <emily.faccin@emily.faccin>
2021-11-23 20:41:45 -0300

    add: client data structure

---
+++ src/bootcamp/data_structs.clj
@@ -0,0 +1,9 @@
+(ns bootcamp.data-structs)
+
+; :client {:name  name,
+;         :cpf cpf,
+;         :email email}
+
+(def client {:name  "Emily",
+             :cpf   "39875649088",
+             :email "emily.faccin@nubank.com.br"})
