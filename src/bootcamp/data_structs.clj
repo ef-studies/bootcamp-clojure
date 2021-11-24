c9bbf005dbbb4c6af72c85eb11d0acbc8fce3046
Emily Faccin <emily.faccin@emily.faccin>
2021-11-23 21:03:33 -0300

    add: credit card data structure

--- src/bootcamp/data_structs.clj
+++ src/bootcamp/data_structs.clj
@@ -1,9 +1,24 @@
 (ns bootcamp.data-structs)
 
-; :client {:name  name,
-;         :cpf cpf,
-;         :email email}
+; :client {:name  string,
+;         :cpf string,
+;         :email string}
 
 (def client {:name  "Emily",
              :cpf   "39875649088",
              :email "emily.faccin@nubank.com.br"})
+
+; ==================================================
+
+; Dado que o exercicio trata de apenas um cliente, nao ha necessidade
+; de trackear o dono do cartao
+
+;{:credit-card {:number string,
+;               :cvv string,
+;               :expiration-date string,
+;               :limit double}}
+
+(def credit-card {:number          "0000 0000 0000 0000",
+                  :cvv             "432",
+                  :expiration-date "2021-11-22",
+                  :limit           1000.0})
