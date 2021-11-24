99a005fd570bec72ab8d52d280ea3d07a189bd71
Emily Faccin <emily.faccin@nubank.com.br>
2021-11-24 18:15:57 -0300

    add: function to calculate total by category

---
+++ src/bootcamp/logic.clj
@@ -0,0 +1,23 @@
+(ns bootcamp.logic
+  (:require [bootcamp.db :as b.db]))
+
+; funcao que retorna o total gasto por categoria
+
+;(defn sum-by-category
+;  [[category purchases]]
+;  {:category category :total (reduce + (map :value purchases))})
+
+(defn sum-by-category
+  [[category purchases]]
+  {:category category,
+   :total    (->> purchases
+                  (map :value)
+                  (reduce +))})
+
+(defn total-by-category
+  [purchases]
+  (->> purchases
+       (group-by :category)
+       (map sum-by-category)))
+
+(println (total-by-category b.db/purchases))
